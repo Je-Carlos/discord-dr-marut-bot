3548aef1714e6b5aea585b80fa5ba455e515e6ff
Jean <jeancotillo@proton.me>
2026-02-27 17:18:56 -0300

    Commit inicial do projeto

---
+++ src/main/java/discord/bot/discordDrMarutBot.java
@@ -0,0 +1,22 @@
+package discord.bot;
+
+import net.dv8tion.jda.api.JDA;
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class discordDrMarutBot
+{
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(discordDrMarutBot.class);
+    private static JDA jda;
+
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
