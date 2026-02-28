2dc9152fbe56f9355461f67b95293397f3381921
jean <jeancotillo@proton.me>
2026-02-28 19:18:40 -0300

    Implementa inicialização do bot com carregamento de token via configuração

--- src/main/java/discord/bot/discordDrMarutBot.java
+++ src/main/java/discord/bot/discordDrMarutBot.java
@@ -1,22 +1,40 @@
 package discord.bot;
-
+import discord.bot.config.BotConfig;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.logging.Logger;
+import java.util.EnumSet;
 
-/**
- * Hello world!
- *
- */
+
 public class discordDrMarutBot
 {
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(discordDrMarutBot.class);
     private static JDA jda;
+    private static final Logger logger = LoggerFactory.getLogger(discordDrMarutBot.class);
 
     public static void main( String[] args )
     {
+        String botToken = BotConfig.getBotToken();
 
-        System.out.println( "Hello World!" );
+        if (botToken == null || botToken.isEmpty())
+        {
+            logger.error("bot token not found in config.properties. Provide a valid token");
+
+        }
+
+        try {
+            jda = JDABuilder.createDefault(botToken)
+                    .enableIntents(EnumSet.allOf(GatewayIntent.class))
+                    // todo: arrumar isso aqui:
+//                    .addEventListeners(new CommandListener())
+                    .build();
+            jda.awaitReady();
+            logger.info("Bot is ready!");
+        } catch (Exception error) {
+            logger.error("error while initializing discord dr marutbot bot", error);
+
+        }
     }
 }
